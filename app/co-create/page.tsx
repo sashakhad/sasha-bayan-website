f484e7efeb8c029095b8a9123d0271b78972679b
Sasha Khadivian <sashabayan@gmail.com>
2025-05-04 14:10:57 -0700

    add google form

---
+++ app/co-create/page.tsx
@@ -0,0 +1,15 @@
+// pages/inspire.tsx
+
+export default function InspirePage() {
+  return (
+    <div className="flex items-center justify-center py-8">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdmbaqEl1w2akx0bc8dBdM6G6sJwMBcKWjyWrUd1D84EI1QVQ/viewform?embedded=true"
+        width="640"
+        height="900"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
